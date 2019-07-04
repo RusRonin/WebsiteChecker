937980630322bf5d5fb18b9f0311ed083564b233
RusRonin <aleksey.pavlovskiy02@mail.ru>
2019-07-04 12:15:32 +0300

    1)Refactoring done 2)Exception handlers added 3)Site's Equals  and GetHashCode methods rewritten 4)Unit tests for Site class added

---
+++ Site.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebsiteChecker
+{
+    public class Site : IComparable<Site>, IComparer<Site>
+    {
+        public string Address { get; set; }
+
+        public Site(string siteAddress)
+        {
+            Address = siteAddress;
+        }
+
+        public int Compare(Site s1, Site s2)
+        {
+            if (s1 == null && s2 == null)
+            {
+                return 0;
+            }
+            if (s1 != null && s2 == null)
+            {
+                return 1;
+            }
+            if (s1 == null && s2 != null)
+            {
+                return -1;
+            }
+            //на этом этапе оба объекта гарантированно != null
+            return String.Compare(s1.Address, s2.Address);
+        }
+        public int CompareTo(Site s)
+        {
+            if (s == null)
+            {
+                return -1;
+            }
+            else
+            {
+                string site1Address = this.Address;
+                return site1Address.CompareTo(s.Address);
+            }
+        }
+
+        public override bool Equals(Object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            Site s = obj as Site;
+            if (s as Site == null)
+            {
+                return false;
+            }    
+            return this.Address == s.Address;
+        }
+        
+        public bool Equals(Site s)
+        {
+            if (s == null)
+            {
+                return false;
+            }
+            return this.Address == s.Address;
+        }
+
+        public override int GetHashCode()
+        {
+
+            int GetNumberFromText(string processingString)
+            {
+                int number = 0;
+                foreach (char symbol in processingString)
+                {
+                    number += (int)symbol;
+                }
+                return number;
+            }
+
+            int hash = 17;
+
+            hash *= GetNumberFromText(this.Address);
+            //при добавлении полей - дописать сюда их хеширование
+
+            return hash;
+        }
+    }
+}
---
+++ SiteListChanger.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace WebsiteChecker
+{
+    internal static class SiteListChanger
+    {
+        private static XmlDocument xmlDoc;
+        private static XmlElement xmlRoot;
+
+        internal static void ConfigureXml()
+        {
+            try
+            {
+                xmlDoc = new XmlDocument();
+                try
+                {
+                    xmlDoc.Load("sites.xml");
+                    xmlRoot = xmlDoc.DocumentElement;
+                }
+                //в случае необнаружения xml файла создается новый.
+                //реализовано средствами linq to xml, так как это единственный найденный мною
+                //способ избежать необнаружения корневого элемента.
+                //этой же цели служит фальшивый адрес и его последущее удаление
+                //за счет сверки с пустым списком
+                catch (FileNotFoundException)
+                {
+                    XDocument xdoc = new XDocument(new XElement("sites",
+                        new XElement("site",
+                            new XAttribute("address", "noaddress")
+                        )));
+                    xdoc.Save("sites.xml");
+                       
+                    xmlDoc.Load("sites.xml");
+                    xmlRoot = xmlDoc.DocumentElement;
+                    List<Site> sites = new List<Site>() { };
+                    CommitChanges(sites);
+                }
+            }
+            catch(XmlException)
+            {
+                UserInterface.PrintString("Возникла ошибка при работе с базой данных. Пожалуйста перезагрузите приложение.", true);
+            }
+            catch(Exception)
+            {
+                UserInterface.PrintString("Возникла ошибка. Пожалуйста перезагрузите приложение", true);
+            }
+            
+        }
+
+        internal static void AddSite(ref List<Site> sites)
+        {
+            UserInterface.PrintString("Введите адрес сайта: ", false);
+            string siteAddress = Console.ReadLine();
+            Site site = new Site(siteAddress);
+
+            if (sites.IndexOf(site) > -1)
+            {
+                UserInterface.PrintString("Данный сайт уже есть в списке", true);
+            }
+            else
+            {
+                sites.Add(site);
+            }
+        }
+
+        internal static void RemoveSite(ref List<Site> sites)
+        {
+            UserInterface.PrintString("Введите адрес сайта: ", false);
+            string siteAddress = Console.ReadLine();
+            Site site = new Site(siteAddress);
+
+            RemoveSite(ref sites, site);
+        }
+
+        internal static void RemoveSite(ref List<Site> sites, Site site)
+        {
+            while (sites.IndexOf(site) > -1)
+            {
+                sites.Remove(site);
+            }
+            UserInterface.PrintString("Данный сайт удален из списка", true);
+        }
+
+        internal static List<Site> LoadSites()
+        {
+            List<Site> sites = new List<Site>() { };
+            try
+            {
+                foreach (XmlNode xmlNode in xmlRoot)
+                {
+                    if (xmlNode.Name == "site" && xmlNode.Attributes.Count > 0)
+                    {
+                        XmlNode attr = xmlNode.Attributes.GetNamedItem("address");
+                        if (attr != null)
+                        {
+                            Site site = new Site(attr.Value);
+                            sites.Add(site);
+                        }
+                    }
+                }
+            }
+            catch(XmlException)
+            {
+                UserInterface.PrintString("Возникла ошибка при работе с базой данных. Пожалуйста перезагрузите приложение.", true);
+            }
+            catch(ArgumentNullException)
+            {
+                UserInterface.PrintString("Возникла ошибка. Пожалуйста перезагрузите приложение", true);
+            }
+            catch(Exception)
+            {
+                UserInterface.PrintString("Возникла ошибка. Пожалуйста перезагрузите приложение", true);
+            }
+            return sites;
+        }
+        internal static void CommitChanges(List<Site> sites)
+        {
+            try
+            {
+                //проверяем сайты на устаревание. устаревшие(удаленные из списка) удаляем из документа, 
+                //не устаревшие удаляем из вспомогательного списка
+                foreach (XmlNode xmlNode in xmlRoot)
+                {
+                    if (xmlNode.Name == "site" && xmlNode.Attributes.Count > 0)
+                    {
+                        XmlNode attr = xmlNode.Attributes.GetNamedItem("address");
+                        if (attr != null)
+                        {
+                            Site site = new Site(attr.Value);
+                            if (sites.IndexOf(site) == -1)
+                            {
+                                xmlRoot.RemoveChild(xmlNode);
+                            }
+                            else
+                            {
+                                RemoveSite(ref sites, site);
+                            }
+                        }
+
+                    }
+                }
+
+                //оставшиеся во вспомогательном списке добавляем в xml документ
+                foreach (Site site in sites)
+                {
+                    XmlElement newSite = xmlDoc.CreateElement("site");
+                    XmlAttribute addressAttribute = xmlDoc.CreateAttribute("address");
+                    XmlText addressValue = xmlDoc.CreateTextNode(site.Address);
+
+                    addressAttribute.AppendChild(addressValue);
+                    newSite.Attributes.Append(addressAttribute);
+                    xmlRoot.AppendChild(newSite);
+
+                }
+                
+                xmlDoc.Save("sites.xml");
+            }
+            catch (XmlException)
+            {
+                UserInterface.PrintString("Возникла ошибка при работе с базой данных. Пожалуйста перезагрузите приложение.", true);
+            }
+            catch (ArgumentNullException)
+            {
+                UserInterface.PrintString("Возникла ошибка. Пожалуйста перезагрузите приложение", true);
+            }
+            catch (Exception)
+            {
+                UserInterface.PrintString("Возникла ошибка. Пожалуйста перезагрузите приложение", true);
+            }
+        }
+    }
+}
